ebd7d15e39cc28834e8a75c539362d2bf3e117a1
Ivan <soldren2@gmail.com>
2022-09-19 22:14:54 +0300

    add objektiv - 1

--- Program.cs
+++ Program.cs
@@ -1,10 +1,10 @@
 ﻿using static System.Console;
 Clear();
 
-WriteLine ("Первая задача");
+WriteLine ("Первая задача дан массив из него нужно получить другой массив [“Hello”, “2”, “world”, “:-)”] → [“2”, “:-)”]");
 string [] array= {"Hello", "2", "world", ":-)"};
 WriteLine();
 WriteLine();
- WriteLine($"[ “{array[1]}”, “{array[3]}” ]");
+WriteLine($"[ “{array[1]}”, “{array[3]}” ]");
 
 
