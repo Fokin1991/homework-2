08faf657baf0ec0f568871f5ff68f5d8f9f1242f
Ivan <soldren2@gmail.com>
2022-09-19 22:05:56 +0300

    add objektiv 1

--- Program.cs
+++ Program.cs
@@ -1,2 +1,10 @@
 ﻿using static System.Console;
 Clear();
+
+WriteLine ("Первая задача");
+string [] array= {"Hello", "2", "world", ":-)"};
+WriteLine();
+WriteLine();
+ WriteLine($"[ “{array[1]}”, “{array[3]}” ]");
+int [] a=new int[5];
+
