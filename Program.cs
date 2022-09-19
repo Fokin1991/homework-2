eca2118540da7031ca7d3d4dadcf78f746d2e5dd
Ivan <soldren2@gmail.com>
2022-09-19 22:26:03 +0300

    add objektiv - 2

--- Program.cs
+++ Program.cs
@@ -6,8 +6,16 @@ string [] array= {"Hello", "2", "world", ":-)"};
 WriteLine();
 WriteLine();
 WriteLine($"[ “{array[1]}”, “{array[3]}” ]");
+WriteLine();
+WriteLine();
 
 
 
-
-
+WriteLine();
+WriteLine ("Вторая задача. ан массив из него нужно получить другой массив [“1234”, “1567”, “-2”, “computer science”] → [“-2”] ");
+string [] array1= {"1234", "1567", "-2", "computer science"};
+WriteLine();
+WriteLine();
+WriteLine($"[ “{array1[2]}” ]");
+WriteLine();
+WriteLine();
